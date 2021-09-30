d410d59035cea0ea12ad03c4404c19ff3b6b54db
hidan12 <marcarfe12@gmail.com>
2021-09-30 03:03:20 -0500

    add file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-  char st [] = "Programming is like building a multilingual puzzle";
+  char st [] = "\"Programming is like building a multilingual puzzle";
   puts (st);
   return 0;
 }

3b7fe329f48984667dc51ebf499864a746bcbdfe
hidan12 <marcarfe12@gmail.com>
2021-10-01 13:02:20 -0500

    add file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num_asc;
+
+	for (num_asc = 48; num_asc != 103; num_asc++)
+	{
+		putchar (num_asc);
+		if (num_asc == 57)
+			num_asc = 97;
+	}
+	putchar (10);
+	return (0);
+}
+

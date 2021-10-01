027b73d3633c0ff2dce3413d377a098199dee500
hidan12 <marcarfe12@gmail.com>
2021-10-01 03:45:15 -0500

    add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>  
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num_asc;
+for (num_asc = 97; num_asc != 123; num_asc++)
+putchar (num_asc);
+putchar (10);
+return (0);
+}

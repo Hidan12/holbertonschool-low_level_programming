b0c3d52baa5568f2d25c0d61b4ecbea683e79b43
hidan12 <marcarfe12@gmail.com>
2021-10-01 12:36:18 -0500

    add file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num)
+	putchar (10);
+	return (0);
+}
+

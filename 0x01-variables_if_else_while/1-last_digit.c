ec20e57f94701b612288572077f3bb26eef56faa
hidan12 <marcarfe12@gmail.com>
2021-10-01 03:15:37 -0500

    add file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,10 +20,12 @@ last_dig = (n % -10);
 else
 last_dig = (n % 10);
 if (last_dig > 5)
-printf("%s %d %s %d %s", "Last digit of", n, "is", last_dig, "and is greater than 5\n");
+printf("%s %d %s %d %s",
+"Last digit of", n, "is", last_dig, "and is greater than 5\n");
 else if (last_dig == 0)
 printf("%s %d %s %d %s", "Last digit of", n, "is", last_dig, "and is 0\n");
 else
-printf("%s %d %s %d %s", "Last digit of", n, "is", last_dig, "and is less than 6 and not 0\n");
+printf("%s %d %s %d %s",
+"Last digit of", n, "is", last_dig, "and is less than 6 and not 0\n");
 return (0);
 }

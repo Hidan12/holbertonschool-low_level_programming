46a35b2dd4bc783fc5e4aab63749ce52f388faec
hidan12 <marcarfe12@gmail.com>
2021-10-01 12:42:32 -0500

    add file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num_asc;
+
+	for (num_asc = 48; num_asc != 58; num_asc++)
+		putchar (num_asc);
+	putchar (10);
+	return (0);
+}
+

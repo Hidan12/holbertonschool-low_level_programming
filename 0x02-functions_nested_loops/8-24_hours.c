1f07643e55c06a23e19d9b5baf6a8952e06a547f
hidan12 <marcarfe12@gmail.com>
2021-10-05 09:28:45 -0500

    fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,17 @@
 
 void jack_bauer(void)
 {
-	int a,b;
+	int a, b;
 
 	for (a = 0; a < 24; a++)
 	{
-		for (b = 0; b > 60; b++)
+		for (b = 0; b < 60; b++)
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
+			_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
 			_putchar(58);
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
+			_putchar('0' + (b / 10));
+			_putchar('0' + (b % 10));
 			_putchar(10);
 		}
 	}

6ff1da3d86b431cc7f2e478b4df359aeae07f8c8
hidan12 <marcarfe12@gmail.com>
2021-10-05 00:25:18 -0500

    add fix 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,12 @@ int print_last_digit(int n)
 	if (n ==  0)
 	{
 		_putchar(48);
-		return (48);
+		return (n + '0');
 	}
 	else if (n < 0)
 		n = n * -1;
 	n = n % 10;
-	n = n + 48;
-	_putchar(n);
-	return (n);
+	_putchar(n + '0');
+	return (n + '0');
 }
 

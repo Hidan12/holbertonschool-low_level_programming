be80d7bf6411261e88a3969e7ca56dea8c2a9ba3
hidan12 <marcarfe12@gmail.com>
2021-10-05 09:56:03 -0500

    fix 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,9 +22,12 @@ void times_table(void)
 				_putchar('0' + (result / 10));
 				_putchar('0' + (result % 10));
 			}
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
+			if (b < 9 )
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
 		}
 		_putchar(10);
 	}

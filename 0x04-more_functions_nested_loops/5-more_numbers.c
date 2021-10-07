2f7e40989be72251028f0d5f356961c886ed0abe
hidan12 <marcarfe12@gmail.com>
2021-10-07 03:27:22 -0500

    add file 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - show the numbers.
+ *
+ */
+void more_numbers(void)
+{
+	int a, b, res;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14 ; b++)
+		{
+			if (b < 10)
+				_putchar(b + '0');
+			else
+			{
+				res = (b / 10);
+				_putchar(res + '0');
+				res = (b % 10);
+				_putchar(res + '0');
+			}
+		}
+		_putchar(10);
+	}
+}

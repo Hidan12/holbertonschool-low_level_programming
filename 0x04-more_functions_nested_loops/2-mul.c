a60785dd9c3d7471f559cc8cdd91ec98fd0ea786
hidan12 <marcarfe12@gmail.com>
2021-10-07 02:19:31 -0500

    add file 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - perform a multiplication
+ *
+ * Return: result multiplication
+ *
+ * @a: variable 1
+ * @b: variable 2
+ */
+int mul(int a, int b)
+{
+	return ((a * b));
+}

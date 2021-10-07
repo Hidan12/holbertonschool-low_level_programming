3420ed8abf5f0904d83335a9f37b0039bacec8ef
hidan12 <marcarfe12@gmail.com>
2021-10-07 02:36:34 -0500

    add file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - show the numbers from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+	_putchar(10);
+}

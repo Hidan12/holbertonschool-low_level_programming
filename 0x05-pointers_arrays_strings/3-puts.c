f82e6d632669a134b7b1d85b9f00931e56762aec
hidan12 <marcarfe12@gmail.com>
2021-10-11 05:25:22 -0500

    add file 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print
+ *
+ * @str: char
+ *
+ * Return: size of s.
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar(10);
+
+}

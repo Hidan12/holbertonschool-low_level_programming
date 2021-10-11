b9bf48a408ccc77a33214644268168552a93c65e
hidan12 <marcarfe12@gmail.com>
2021-10-11 16:52:33 -0500

    fix 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,29 @@
 #include "main.h"
 /**
  * rev_string - print
- *
  * @str: char
- *
  */
 void rev_string(char *str)
 {
-	int a, b;
+	int a, b, mit;
+	char sus;
 
 	for (a = 0; str[a] != '\0'; a++)
 		continue;
 	a--;
-	for (b = a; str[b] != '\0'; b--)
+	if (a / 2 != 0)
+		mit = (a + 1) / 2;
+	else
+		mit = (a / 2);
+	for (b = 0; b < a; b++)
 	{
-		_putchar(str[b]);
+		if (b < mit)
+		{
+			sus = str[b];
+			str[b] = str[(a - b)];
+			str[(a - b)] = sus;
+		}
+		else
+			break;
 	}
-	_putchar(10);
-
 }

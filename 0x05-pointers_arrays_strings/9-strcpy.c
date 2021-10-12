e8dfef9035a9ff344d3950e50de13f0fc42db81f
hidan12 <marcarfe12@gmail.com>
2021-10-11 23:19:24 -0500

    fix 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _strcpy - swap
+ * *_strcpy - swap
  *
  * @src: pointer
- * @dest: pointer.
+ *
+ * @dest: pointer
+ *
+ * Return: pointer.
  */
 char *_strcpy(char *dest, char *src)
 {
 	int a;
+
 	for (a = 0; src[a] != '\0'; a++)
 		dest[a] = src[a];
 	return (dest);

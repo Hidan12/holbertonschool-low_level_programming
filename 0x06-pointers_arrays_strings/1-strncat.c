cf14507dfdb9cd5ce91b42703c1a027b0bcef4fd
hidan12 <marcarfe12@gmail.com>
2021-10-13 16:27:14 -0500

    add file 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *_strncat - join two pointers
+ *
+ * @dest: pointer1
+ *
+ * @src: pointer2
+ *
+ * @n: amount
+ *
+ * Return: the union two pointer.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int tam_1, tam_2, un;
+
+	for (tam_1 = 0; dest[tam_1] != '\0'; tam_1++)
+		continue;
+	for (tam_2 = 0; src[tam_2] != '\0'; tam_2++)
+		continue;
+	for (un = 0; dest[un + 1] != '\0' ; un++, tam_1++)
+	{
+		dest[tam_1] = src[un];
+		if ((n > tam_2 && un == tam_2) || (n < tam_2 && un < n))
+			break;
+	}
+	return (dest);
+}

5a08b23ae0b8ee982fb5d06f74ddad929ab0e525
hidan12 <marcarfe12@gmail.com>
2021-10-14 18:26:21 -0500

    add file 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,8 +10,18 @@
 
 void reverse_array(int *a, int n)
 {
-	for (;n >= 0; n--)
+	int b = 0, sus, c;
+
+	n--;
+	if (n % 2 != 0)
+		c = (n + 1) / 2;
+	else
+		c = n / 2;
+	for (; n >= c; n--)
 	{
-		_putchar(a[n]);
+		sus = a[b];
+		a[b] = a[n];
+		a[n] = sus;
+		b++;
 	}
 }

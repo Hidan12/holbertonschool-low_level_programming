fc1945fec9f93617052077ab54fb43aa97717a40
hidan12 <marcarfe12@gmail.com>
2021-10-14 18:38:11 -0500

    add fix 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int tam_1, tam_2, un;
+	int tam_1, tam_2, i;
 
 	for (tam_1 = 0; dest[tam_1] != '\0'; tam_1++)
 		continue;
 	for (tam_2 = 0; (src[tam_2] != '\0'); tam_2++)
 		continue;
-	for (un = 0; dest[un + 1] != '\0' ; un++, tam_1++)
-	{
-		dest[tam_1] = src[un];
-		if ((n > tam_2 && un == tam_2) || (n < tam_2 && un < n))
-			break;
-	}
-	dest[tam_1 + un] = '\0';
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[tam_1 + i] = src[i];
+	dest[tam_1 + i] = '\0';
 	return (dest);
 }

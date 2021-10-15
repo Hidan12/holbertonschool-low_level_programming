9e17ec59b575624fcf1a051a79aa5c567340ab1b
hidan12 <marcarfe12@gmail.com>
2021-10-15 00:22:03 -0500

    fix 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,15 +8,15 @@
  */
 char *cap_string(char *a)
 {
-	char let,c[] = "44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9";
+	char let, c = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 	int num_asc, pos, pos_c;
 
 	for (pos = 0; a[pos] != '\0'; pos++)
 	{
 		num_asc = (char)a[pos];
-		if(pos == 0 && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
+		if (pos == 0 && (a[pos] >= 97 && a[pos] <= 122))
 		{
-			a[pos] = a[pos] -32;
+			a[pos] = a[pos] + 32;
 		}
 
 		for (pos_c = 0; c[pos_c] != '\0'; pos_c++)
@@ -24,10 +24,10 @@ char *cap_string(char *a)
 			if (a[pos] == c[pos_c] && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
 			{
 				num_asc = (char)a[pos + 1];
-			       	let = (num_asc - 32);
-		       		a[pos + 1] = let;
-	       		}
-       		}
+				let = (num_asc - 32);
+				a[pos + 1] = let;
+			}
+		}
 	}
 	return (a);
 }

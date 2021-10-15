8819174bff4ff52f0e583b0f9df5af4c4c9ac430
hidan12 <marcarfe12@gmail.com>
2021-10-14 22:54:25 -0500

    add fix 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,28 +9,21 @@
 char *cap_string(char *a)
 {
 	char let;
-	int num_asc, pos, pos_1, cont = 0;
+	int num_asc, pos;
 
 	for (pos = 0; a[pos] != '\0'; pos++)
 	{
 		num_asc = (char)a[pos];
-		if ((cont == 0 || a[pos - 1] == ' ' || a[pos - 1] == '.')
+		if ((pos == 0 || a[pos - 1] == ' ' || a[pos - 1] == '.')
 		    && (num_asc <= 122 && num_asc >= 97))
 		{
-			cont = 1;
 			let = (num_asc - 32);
 			a[pos] = let;
 		}
-		else if (a[pos] == 92 && (a[pos + 1] == 'n' || a[pos + 1] == 't'))
+		else if(a[pos - 1] == '\n')
 		{
-			cont = 0;
-			if (a[pos + 1] == 'n')
-				a[pos] = 10;
-			else
-				a[pos] = ' ';
-			for (pos_1 = (pos + 1); a[pos_1] != '\0'; pos_1++)
-				a[pos_1] = a[pos_1 + 1];
-			a[pos_1 + 1] = '\0';
+			let = (num_asc - 32);
+			a[pos] = let;
 		}
 	}
 	return (a);

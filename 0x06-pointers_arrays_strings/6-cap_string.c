665679738b04c8021a407ce1982172f53034579c
hidan12 <marcarfe12@gmail.com>
2021-10-14 23:56:44 -0500

    fixxx6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,26 @@
  */
 char *cap_string(char *a)
 {
-	char let;
-	int num_asc, pos;
+	char let,c[] = "44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9";
+	int num_asc, pos, pos_c;
 
 	for (pos = 0; a[pos] != '\0'; pos++)
 	{
 		num_asc = (char)a[pos];
-		if ((pos == 0 || a[pos - 1] == ' ' || a[pos - 1] == '.')
-		    && (num_asc <= 122 && num_asc >= 97))
+		if(pos == 0 && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
 		{
-			let = (num_asc - 32);
-			a[pos] = let;
+			a[pos] = a[pos] -32;
 		}
-		else if (a[pos - 1] == '\n')
+
+		for (pos_c = 0; c[pos_c] != '\0'; pos_c++)
 		{
-			let = (num_asc - 32);
-			a[pos] = let;
-		}
-		else if (a[pos] == '\t')
-			a[pos] = ' ';
+			if (a[pos] == c[pos_c] && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
+			{
+				num_asc = (char)a[pos + 1];
+			       	let = (num_asc - 32);
+		       		a[pos + 1] = let;
+	       		}
+       		}
 	}
 	return (a);
 }

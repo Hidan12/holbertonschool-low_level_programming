1d609c9d66f5254dfd54cf83c3976314c0d8fb08
hidan12 <marcarfe12@gmail.com>
2021-10-15 00:44:01 -0500

    add test 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
  */
 char *cap_string(char *a)
 {
-	char let, c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	char let, *ch = ".,\n\t:;";
 	int num_asc, pos, pos_c;
 
 	for (pos = 0; a[pos] != '\0'; pos++)
@@ -19,9 +19,9 @@ char *cap_string(char *a)
 			a[pos] = a[pos] + 32;
 		}
 
-		for (pos_c = 0; c[pos_c] != '\0'; pos_c++)
+		for (pos_c = 0; ch[pos_c] != '\0'; pos_c++)
 		{
-			if (a[pos] == c[pos_c] && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
+			if (a[pos] == ch[pos_c] && (a[pos + 1] >= 97 && a[pos + 1] <= 122))
 			{
 				num_asc = (char)a[pos + 1];
 				let = (num_asc - 32);

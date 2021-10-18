c56c65109e7cb941f9f4606ae53989270c966bc8
hidan12 <marcarfe12@gmail.com>
2021-10-18 12:40:50 -0500

    add 2 file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - character finder in a string
+ *
+ * @s: string
+ *
+ * Return: the string from where the character is found.
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; *s != '\0'; a++)
+	{
+		if (*s == c)
+			return(s);
+		s++;
+	}
+	return (s);
+}

60f949d045d4c8e3916581b1d981bf2b3528b43c
hidan12 <marcarfe12@gmail.com>
2021-10-19 23:23:38 -0500

    add file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - character count
+ *
+ * @s: string
+ *
+ * @accept: character
+ *
+ * Return: the number of bytes in the initial segment.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, sum;
+	sum = 0;
+	for (; *s != '\0'; s++)
+	{
+		if ((*s <= 90 && *s >= 65) || (*s <= 122 && *s >= 97))
+		{
+			for (a = 0; accept[a] != '\0'; a++)
+			{
+				if (*s == accept[a])
+					sum++;
+			}
+		}
+		else
+			break;
+	}
+	return (sum);
+}

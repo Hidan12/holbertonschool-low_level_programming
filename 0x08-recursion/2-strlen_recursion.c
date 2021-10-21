86f616c34a19990bf18b38b80f75412fa8f0d1ea
hidan12 <marcarfe12@gmail.com>
2021-10-20 19:39:55 -0500

    add file 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - size string
+ *
+ * @s: string
+ *
+ * Return - size
+ */
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s != '\0')
+	{
+		a += _strlen_recursion(s + 1);
+		return (a += 1);
+	}
+	else
+		return (0);
+}

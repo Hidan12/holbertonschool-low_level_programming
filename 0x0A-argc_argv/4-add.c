c8d8c703d6fba3545007bbc6b81294f586e961de
hidan12 <marcarfe12@gmail.com>
2021-10-25 17:49:54 -0500

    add file 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: string size
+ *
+ * @argv: string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, a, num, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (a = 0; argv[i][a] != '0'; a++)
+			{
+				num = argv[i][a];
+				if (!(num > 47 && num < 58))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+	return (0);
+}

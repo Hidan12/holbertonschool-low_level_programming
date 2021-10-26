06a30d6330accd028ad104965eb43f558bf9cd1e
hidan12 <marcarfe12@gmail.com>
2021-10-26 17:14:25 -0500

    add fix 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * swap_int - swap
+ * *create_array - create array
  *
  * @size: pointer
- * @c: pointer.
+ * @c: char
  */
 char *create_array(unsigned int size, char c)
 {
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	if (size != 0)
 	{
 		ptr = malloc(sizeof(char) * size);
+		if (ptr == '\0')
+			return ('\0');
 		for (a = 0; a <= size; a++)
 			ptr[a] = c;
 		return (ptr);

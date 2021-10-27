a78eefc7a607dba1acca683a7683171c488a3aa5
hidan12 <marcarfe12@gmail.com>
2021-10-27 00:58:58 -0500

    add fix 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,13 @@ int **alloc_grid(int width, int height)
 		ptr = (int **)malloc(width * sizeof(int *));
 		if (ptr == NULL)
 			return ('\0');
-		for (a = 0; a <= width; a++)
+		for (a = 0; a < width; a++)
 		{
 			ptr[a] = (int *)malloc(height * sizeof(int));
 			if (ptr[a] == NULL)
 				return ('\0');
 		}
+		ptr[a+1] = '\0'
 		return (ptr);
 	}
 	else

845e492dec0ec2a660901ba26ffc74176362719a
hidan12 <marcarfe12@gmail.com>
2021-10-27 10:52:00 -0500

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,14 +10,13 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **ptr, a, b;
+	int **ptr, a, b, c;
 
-	if ((width <= 0 || height <= 0))
+	if (!(width <= 0 || height <= 0))
 	{
 		ptr = (int **)malloc(width * sizeof(int *));
 		if (ptr == NULL)
 		{
-			free(ptr);
 			return ('\0');
 		}
 		for (a = 0; a <= width; a++)
@@ -25,14 +24,20 @@ int **alloc_grid(int width, int height)
 			ptr[a] = (int *)malloc(height * sizeof(int));
 			if (ptr[a] == NULL)
 			{
+				for (c = 0; c <= a; c++)
+				{
+					free(ptr[c]);
+				}
 				free(ptr);
-				return ('\0');
+				return('\0');
 			}
 		}
 		for (a = 0; a < width; a++)
 		{
 			for (b = 0; b < height; b++)
+			{
 				ptr[a][b] = 0;
+			}
 		}
 		return (ptr);
 	}

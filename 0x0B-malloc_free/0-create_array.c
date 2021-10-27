42d55215be7a61eaa9ce1f98b2fb82ab2e225492
hidan12 <marcarfe12@gmail.com>
2021-10-26 19:26:58 -0500

    add fix 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ptr;
+	char *ptr = NULL;
 	unsigned int a;
 
 	if (size != 0)
 	{
 		ptr = malloc(sizeof(char) * size);
+		if (ptr == NULL)
+			return ('\0');
 		for (a = 0; a < size; a++)
 		{
 			ptr[a] = c;

d55abfa998a8a485eea41b55f89355b8a31160d4
hidan12 <marcarfe12@gmail.com>
2021-10-26 19:40:25 -0500

    add file 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *create_array - create array
+ *
+ * @size: pointer
+ * @c: char
+ * Return: Pointer.
+ */
+char *_strdup(char *str)
+{
+	char *ptr = NULL;
+	unsigned int a, size;
+
+	for (size = 0; str[size] != '\0'; size++)
+		continue;
+	if (size != 0)
+	{
+		ptr = malloc(sizeof(char) * size);
+		if (ptr == NULL)
+			return ('\0');
+		for (a = 0; a < size; a++)
+		{
+			ptr[a] = str[a];
+		}
+		return (ptr);
+	}
+	return ('\0');
+}

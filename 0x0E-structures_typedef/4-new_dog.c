6088cc7edaf94e0b323c3dfaa0523c1a03f3dab4
hidan12 <marcarfe12@gmail.com>
2021-11-01 16:28:49 -0500

    tes

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - print dog attributes
+ *
+ * @d: structure at the function
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return(NULL);
+	if (name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(ptr);
+		return(NULL);
+	}
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return(ptr);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,5 @@ void print_dog(struct dog *d);
  * dog - strucut with typedef
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif

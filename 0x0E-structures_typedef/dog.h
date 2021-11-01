78c8f2e49236141b66a6aad6a2a424f4cb128c2f
hidan12 <marcarfe12@gmail.com>
2021-11-01 10:58:27 -0500

    add dog.h and main 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog attributes
+ *
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: dog's owner name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

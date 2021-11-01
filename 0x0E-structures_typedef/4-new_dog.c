aa5bc4f72b90d65f0be20670ec20568e4a48b865
hidan12 <marcarfe12@gmail.com>
2021-11-01 17:50:47 -0500

    tes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,30 +1,13 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * *_strcpy - swap
+ * *new_dog - add
  *
- * @src: pointer
- *
- * @dest: pointer
- *
- * Return: pointer.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int a;
-
-	for (a = 0; src[a] != '\0'; a++)
-		dest[a] = src[a];
-	dest[a] = '\0';
-	return (dest);
-}
-/**
- * print_dog - print dog attributes
- *
- * @d: structure at the function
- *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return : ptr
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -33,28 +16,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return(NULL);
-        ptr->name = malloc(sizeof(name));
-	if (ptr->name != NULL)
+	if (name == NULL)
 	{
-		ptr->name = _strcpy( ptr->name, name);
-	}
-	else
-	{
-		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
-	ptr->owner = malloc(sizeof(owner));
-	if (ptr->owner != NULL)
+	if (owner == NULL)
 	{
-		ptr->owner = _strcpy( ptr->owner, owner);
-	}
-	else
-	{
-		free(ptr->owner);
 		free(ptr);
-		return (NULL);
+		return(NULL);
 	}
+	ptr->name = name;
 	ptr->age = age;
+	ptr->owner = owner;
 	return(ptr);
 }

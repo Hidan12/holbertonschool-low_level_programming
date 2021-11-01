ce6ea20961f9ba4132a394183d9f3cb2ceff3857
hidan12 <marcarfe12@gmail.com>
2021-11-01 14:40:59 -0500

    test 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,8 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog strucut with typedef
+ */
+typedef struct dog dog_t;
 #endif

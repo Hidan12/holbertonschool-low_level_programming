d515b7202890a9031036b10748c3909cfbc457a8
hidan12 <marcarfe12@gmail.com>
2021-11-03 18:35:32 -0500

    add test 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
-	if (array != NULL)
+	if (array != NULL && size != 0)
 	{
 		for (a = 0; a < size; a++)
 		{

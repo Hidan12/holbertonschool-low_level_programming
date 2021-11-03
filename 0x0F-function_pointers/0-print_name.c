bd293abc0d45192dedb80e779225d713a18249ef
hidan12 <marcarfe12@gmail.com>
2021-11-03 17:12:33 -0500

    add readme and file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - pass the name as parameter
+ * @name: name of the person
+ * @f: Function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

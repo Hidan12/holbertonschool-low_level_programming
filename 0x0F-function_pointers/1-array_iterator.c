25a069aae65347b422dd3e8917c227e27c3b084e
hidan12 <marcarfe12@gmail.com>
2021-11-03 18:32:07 -0500

    add file 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - go from a number to a function
+ * @array: elements to pass to the function
+ * @size: number of elements
+ * @action: function
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	if (array != NULL)
+	{
+		for (a = 0; a < size; a++)
+		{
+			action(array[a]);
+		}
+	}
+}

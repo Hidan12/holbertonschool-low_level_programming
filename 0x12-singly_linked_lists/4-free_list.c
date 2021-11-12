a43aa2e5808bd009e0fcab15bfd944391e86166b
hidan12 <marcarfe12@gmail.com>
2021-11-12 01:35:55 -0500

    add file 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,17 @@
+#include "lists.h"
 /**
  * free_list - free list
  * @h: node.
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *node;
+
+	while(head)
+	{
+		node = head;
+		free(node->str);
+		free(node);
+		node = head;
+	}
 }

370de8a55dc7712ffc9ef109791a1de9ab51bc54
hidan12 <marcarfe12@gmail.com>
2021-11-11 23:49:03 -0500

    add file 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: node.
+ * Return: size of node.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *n;
+
+	n = h;
+	for (; n != NULL; count++)
+	{
+		if (n->str != NULL)
+			printf("[%d] %s\n", n->len, n->str);
+		else
+			printf("[0] (nil)");
+		n = n->next;
+	}
+	return (count);
+}

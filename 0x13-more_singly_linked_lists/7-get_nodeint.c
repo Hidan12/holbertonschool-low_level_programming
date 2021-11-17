a62bb1ab78e02fba7955810df2a631ebafe29258
hidan12 <marcarfe12@gmail.com>
2021-11-17 01:14:45 -0500

    add file 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - sarch the node
+ * @head: node
+ * @index: node position
+ * Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *nodo;
+	unsigned int count = 0;
+
+	nodo = head;
+	for (;nodo;count++)
+	{
+		if (count == index)
+			return(nodo);
+		nodo = head->next;
+	}
+	return (NULL);
+}

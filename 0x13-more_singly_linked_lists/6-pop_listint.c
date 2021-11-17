856d94ed6de22a443be60567b8489d856f5a8d23
hidan12 <marcarfe12@gmail.com>
2021-11-17 00:20:27 -0500

    add file 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - function with one argument
+ * @head: pointer to head pointer of list
+ * Return: delete element
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *nodo;
+
+	if (*head)
+	{
+		n = (*head)->n;
+		nodo = (*head)->next;
+		free(*head);
+		*head = nodo;
+	}
+	return (n);
+}

b34f0804c4b3a87565c74185a9952a48dcb4488c
hidan12 <marcarfe12@gmail.com>
2022-01-07 15:03:54 -0500

    add file 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *  sum_dlistint - som the element
+ * @head: pointer to the start of the linked list
+ *
+ * Return: node
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *f = head;
+	int sum = 0;
+
+	if (f != NULL)
+	{
+		for (; f != NULL; sum += f->n)
+		{
+			f = f->next;
+			if (f == NULL)
+				break;
+		}
+	}
+	return (sum);
+}
